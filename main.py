27f43aecead6a9510958b319aed945b813eb5b8e
d.pogoncev <d.pogoncev@ktsstudio.ru>
2025-11-15 19:17:42 +0300

    allow all cors

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=store.config.app.origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
